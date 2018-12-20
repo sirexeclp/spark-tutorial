b848a452d99ad7ef4978a14cbb1fd508ad42a1c2
sirexeclp <sirexeclp@gmail.com>
2018-12-20 17:00:55 +0100

    groupby magic

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -3,6 +3,7 @@ package de.hpi.spark_tutorial
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+import org.apache.spark.sql.functions.explode
 
 object Sindy{
 
@@ -17,7 +18,7 @@ object Sindy{
      ) // as data sets
 
     val results = ListBuffer[(String,String)]()
-
+     import spark.implicits._
      for (dataset <- data){
        val column_names = dataset.columns
         for (row:Row <- dataset.collect()) {
@@ -26,6 +27,11 @@ object Sindy{
           }
         }
      }
+     import org.apache.spark.sql.functions.udf
+     import org.apache.spark.sql.functions._
+     val flatten = udf((xs: Seq[Seq[String]]) => xs.flatten)
+     val resDF = spark.createDataFrame(results)
+       resDF.groupBy($"_1").agg(collect_set($"_2")).groupBy($"collect_set(_2)").agg(collect_set($"_1")).filter(_.getSeq(0).length > 1).show(100)
 
      //val test = data.map(dataset => (dataset, dataset.columns))
 
