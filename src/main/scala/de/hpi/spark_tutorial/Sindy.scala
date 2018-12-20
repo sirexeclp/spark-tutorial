520f23d5147acecc9a38a7744219f80e3c4947d0
Gauxi <44170889+Gauxi@users.noreply.github.com>
2018-12-20 15:01:52 +0100

    schritt 1

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -2,17 +2,35 @@ package de.hpi.spark_tutorial
 
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+
 object Sindy{
 
    def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
 
-     val data = spark.read
-       .option("inferSchema", "true")
-       .option("header", "true")
-       .option("delimiter",";")
-       .csv(inputs:_*)
+     val data = inputs.map(
+       spark.read
+         .option("inferSchema", "false")
+         .option("header", "true")
+         .option("delimiter", ";")
+         .csv(_)
+     ) // as data sets
 
-     print(data.count())
-     data.show(100)
+    val results = ListBuffer[(String,String)]()
+
+     for (dataset <- data){
+       val column_names = dataset.columns
+        for (row:Row <- dataset.collect()) {
+          for (j <- row.toSeq.indices) {
+            results += ((row.getString(j), column_names(j)))
+          }
+        }
+     }
+
+     //val test = data.map(dataset => (dataset, dataset.columns))
+
+     //print(data.count())
+      //data.show(100)
+
   }
 }
